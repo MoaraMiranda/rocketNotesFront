ee194ad58a96120f67f73f132d2448017cd1aeb9
moara <moaramirandass@gmail.com>
2023-08-25 12:14:25 +0200

    ButtonText component added

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,11 +4,13 @@ import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Remove note" />
       <Section title="Useful links">
         <Links>
           <li>
@@ -21,9 +23,8 @@ export function Details() {
       </Section>
 
       <Section title="Markers">
-        <Tag title="express"  />
-        <Tag title="nodejs"  />
-
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
       <Button title="Back" />
     </Container>
