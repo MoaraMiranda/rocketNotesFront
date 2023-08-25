96166bda81982f57e58b3870331be6585e82bf6f
moara <moaramirandass@gmail.com>
2023-08-25 12:01:03 +0200

    Tag component added

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -17,6 +18,12 @@ export function Details() {
             <a href="#">https://www.rocketseat.com.br/</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Markers">
+        <Tag title="express"  />
+        <Tag title="nodejs"  />
+
       </Section>
       <Button title="Back" />
     </Container>
