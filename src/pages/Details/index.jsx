ca83ecad9fb42039eb18c5d741f22c10c5f5625f
moara <moaramirandass@gmail.com>
2023-08-25 11:38:52 +0200

    Section component added

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,24 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
-import { Header } from "../../components/Header"
-import { Button } from "../../components/Button"
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
-    <Container>  
-      <Header/>   
-      <Button title="Voltar" loading/>
+    <Container>
+      <Header />
+      <Section title="Useful links">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+      <Button title="Back" />
     </Container>
   );
 }
