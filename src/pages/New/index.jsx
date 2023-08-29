1e078d56110cb04e1802fa4b1cde600c5f918728
moara <moaramirandass@gmail.com>
2023-08-29 13:47:47 +0200

    Added New note page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,21 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import { Container, Form } from "./styles";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Create note</h1>
+            <a href="/">Back</a>
+          </header>
+          <Input placeholder="Title"  />
+        </Form>
+      </main>
+    </Container>
+  );
+}
