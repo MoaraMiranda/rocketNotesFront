29f206fd39e006789dd0a63fecd2c506eddf988f
moara <moaramirandass@gmail.com>
2023-08-29 12:08:27 +0200

    Adding Profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,32 @@
+import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src="https://github.com/moaramiranda.png" alt="User Picture" />
+        <label htmlFor="avatar">
+          <FiCamera  />
+          <input id="avatar" type="file"  />
+        </label>
+        </Avatar>
+        <Input placeholder="Name" type="text" icon={FiUser} />
+        <Input placeholder="Email" type="text" icon={FiMail} />
+        <Input placeholder="Current password" type="Password" icon={FiLock} />
+        <Input placeholder="New password" type="Password" icon={FiLock} />
+        <Button title="Save" />
+      </Form>
+    </Container>
+  );
+}
